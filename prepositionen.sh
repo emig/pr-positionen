76b9e062812a4e23fdfe022de4fc7808f084b410
emiguez <ernesto.miguez@asquera.de>
2015-05-13 19:00:12 +0200

    add ANSI escape color codes

--- prepositionen.sh
+++ prepositionen.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 while true;
   do
     challenge=$(cat verb.txt | sed '/^$/d' | gshuf -n1 | cut -f1)
@@ -8,9 +11,9 @@ while true;
     do
       solution="$(cat < verb.txt | egrep "^$challenge" | cut -f2)"
       if [[ $solution == *$prep* ]]; then
-        echo 'ya!'
+        echo -e "${GREEN}ya!${NC}"
       else
-        echo "nein! $solution != $prep"
+        echo -e "${RED}nein!${NC} ${GREEN}$solution${NC} != ${RED}$prep${NC}"
       fi
       break
     done
